911abd349b4a152699fd647e38524808d175be72
spacewalkingninja <spacewalkingninja@gmail.com>
2024-02-07 02:24:43 +0000

    Update controltdepth.setup.bat

--- executables/Windows/controltdepth.setup.bat
+++ executables/Windows/controltdepth.setup.bat
@@ -1,8 +1,3 @@
 @ECHO OFF
-:NOSTART
-ECHO %sucess%%model_name% Instalation Completed!%ansi_end%
-ECHO %info_h2%model name  : %model_name%%ansi_end% 
-:: PAUSE FOR DEBUG
-IF %arg2_bool% EQU 0 exit
-PAUSE
+ECHO Instalation Completed!
 exit
